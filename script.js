9b8af50375aff82ac30dfb37fa36d2463d60c7f8
ALW-N <alwintomy11@gmail.com>
2023-09-16 22:50:37 +0530

    Commit-2

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const fetchBooksButton = document.getElementById("fetchBooks");
-    const bookListDiv = document.getElementById("bookList");
-
-    fetchBooksButton.addEventListener("click", function () {
-        const xhr = new XMLHttpRequest();
-        const url = "books.json";
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                const books = JSON.parse(xhr.responseText);
-                displayBooks(books);
-            }
-        };
-        xhr.send();
-    });
-
-    function displayBooks(books) {
-        bookListDiv.innerHTML = "";
-        const ul = document.createElement("ul");
-        books.forEach(function (book) {
-            const li = document.createElement("li");
-            li.textContent = book.title + " by " + book.author + " " + book.genre + " " + book.publication_year + " " + book.rating;
-            ul.appendChild(li);
-        });
-        bookListDiv.appendChild(ul);
+const url = 'https://fictional-book-data-scraper-api.p.rapidapi.com/books/%7mystery%7D'; // Replace with the specific genre
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'd7321cad77msh405233d0031c669p18ce7djsnba75e2ed1451',
+        'X-RapidAPI-Host': 'fictional-book-data-scraper-api.p.rapidapi.com'
     }
-});
+};
+console.log(url)
+
+async function fetchAndDisplayData() {
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+
+        if (response.ok) {
+            const tableBody = document.querySelector('#bookTable tbody');
+
+            data.forEach(book => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${book.title}</td>
+                    <td>${book.price}</td>
+                    <td>${book.stock}</td>
+                    <!-- Add more table data cells as needed -->
+                `;
+                tableBody.appendChild(row);
+            });
+
+            // Show the table when data is available
+            document.querySelector('#bookTable').style.display = 'table';
+        } else {
+            console.error(`API request failed with status ${response.status}`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Add a click event listener to the button
+document.querySelector('#fetchButton').addEventListener('click', fetchAndDisplayData);
